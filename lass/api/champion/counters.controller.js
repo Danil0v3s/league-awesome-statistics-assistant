515d0e3122a473c64831460abe609c7fcf6c78a8
Danilo Menezes <danilo.menezes@sky.uk>
2020-03-17 15:21:16 +0000

    change counter method

--- lass/api/champion/counters.controller.js
+++ lass/api/champion/counters.controller.js
@@ -8,34 +8,39 @@ exports.fetchCounters = async (req, res) => {
         const result = await lass.collection('matches').aggregate([
             { '$match': { 'participants.championId': parseInt(championId), 'seasonId': parseInt(seasonId) } },
             { '$project': { gameVersion: 1, participants: 1 } },
-            { '$sort': { gameCreation: -1 } },
-            { '$limit': 5000 }
+            { '$sort': { gameCreation: -1 } }
         ], {
             allowDiskUse: true,
         }, null).toArray();
 
         const grouped = _.groupBy(result.map(r => {
             const champion = r.participants.filter(p => p.championId === parseInt(championId))[0]
-            const oponent = r.participants.filter(p => p.participantId === (champion.participantId < 6 ? champion.participantId + 5 : champion.participantId - 5))[0]
+            const oponent = r.participants.filter(p => p.timeline.lane === champion.timeline.lane && p.teamId !== champion.teamId)[0]
 
-            return {
-                win: oponent.stats.win,
-                championId: oponent.championId
-            }
-        }), 'championId')
-
+            if (oponent) {
+                return {
+                    target: champion.championId,
+                    win: oponent.stats.win,
+                    championId: oponent.championId,
+                    lane: oponent.timeline.lane,
+                    role: oponent.timeline.role
+                }
+            } else return undefined
+        }).filter(e => e), 'championId')
 
         const mapped = _.orderBy(Object.keys(grouped).map(key => {
             const counter = grouped[key]
             const wins = counter.reduce((acc, cur) => cur.win === true ? ++acc : acc, 0)
             const loses = counter.reduce((acc, cur) => cur.win === false ? ++acc : acc, 0)
+            const totalGames = wins + loses
+
             return {
                 championId: counter[0].championId,
-                ratio: wins / (loses + wins) * 100,
-                wins,
-                loses
+                target: counter[0].target,
+                ratio: wins / (totalGames) * 100,
+                wins, loses, totalGames
             }
-        }), ['ratio'], ['desc'])
+        }), ['totalGames', 'wins', 'loses'], ['desc', 'desc', 'asc'])
 
         return res.send(mapped)
     }
